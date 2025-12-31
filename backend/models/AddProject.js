60517242612f4f7298747a4d6d4aafa8003ce679
(っ◔◡◔)っ ♥ bhanu ♥ <83598884+bhanuprasadsamal@users.noreply.github.com>
2025-12-31 12:45:56 +0530

    Project update

--- backend/models/AddProject.js
+++ backend/models/AddProject.js
@@ -2,11 +2,12 @@ import mongoose from 'mongoose';
 
 const projectSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    class: { type: String, required: true },
-    subject: { type: String, required: true },
-    dueDate: { type: String, required: true },
-    description: { type: String, required: true },
+    title: String,
+    class: String,        // e.g. "10"
+    section: String,      // e.g. "A"
+    subject: String,
+    dueDate: String,
+    description: String,
     submissions: { type: Number, default: 0 },
     totalStudents: { type: Number, default: 30 },
   },
