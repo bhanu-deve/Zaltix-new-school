b64ac3481e8ec7118074e6aa8c891c153713bea8
(っ◔◡◔)っ ♥ bhanu ♥ <83598884+bhanuprasadsamal@users.noreply.github.com>
2026-01-31 10:32:19 +0530

    api check

--- sphere-entry-animated-home-main/src/api/api.ts
+++ sphere-entry-animated-home-main/src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "http://20.204.205.244:3000",
   headers: {
     "Content-Type": "application/json"
   }
