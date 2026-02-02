b8aa53b5f2865ec005c0c6449124694596850a87
(っ◔◡◔)っ ♥ bhanu ♥ <83598884+bhanuprasadsamal@users.noreply.github.com>
2026-02-02 12:10:18 +0530

    payment test2

--- backend/config/razorpay.js
+++ backend/config/razorpay.js
@@ -9,5 +9,8 @@ const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
+console.log("KEY:", process.env.RAZORPAY_KEY_ID);
+console.log("SECRET:", process.env.RAZORPAY_KEY_SECRET);
+
 
 export default razorpay;
