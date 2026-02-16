1a5fd9bec8bd8e2839eff611f1d4bb14a0dfb880
(っ◔◡◔)っ ♥ bhanu ♥ <83598884+bhanuprasadsamal@users.noreply.github.com>
2026-02-16 11:21:10 +0530

    check3

--- backend/index.js
+++ backend/index.js
@@ -97,8 +97,11 @@ io.on("connection", (socket) => {
 
 /* MIDDLEWARE */
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// 🔥 FIX FOR LARGE BASE64 IMAGES
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 /* ✅ DOCKER HEALTHCHECK (SERVER ONLY) */
 app.get("/health", (req, res) => {
